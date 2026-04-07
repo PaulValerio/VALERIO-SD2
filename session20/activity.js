653dc300088544d2e276ee174c8d129bfe783702
PaulValerio <paulashleevalerio@gmail.com>
2026-04-07 23:28:14 +0800

    Submitted the activity

--- session20/activity.js
+++ session20/activity.js
@@ -2,7 +2,7 @@ let friends = [];
 
 function showFriends() {
   if (friends.length <= 0) {
-    console.log("No man is a island!");
+    console.log("No man is an island!");
   } else {
     console.log(`You Friend/s are: ${friends}`);
   }
@@ -15,13 +15,17 @@ function addFriend(name) {
     console.log(`${name} is already your friend!`);
   } else {
     friends.push(name.toUpperCase());
-    console.log(`You added ${name.toUpperCase()}.`);
+    console.log(`You added ${name}.`);
   }
 }
 
 function removeFriend(name) {
-  let indexOfFriend = friends.indexOf(name.toUpperCase());
-  if (friends == -1) {
-    friends.splice(indexOfFriend, 1);
+  let indexofFriend = friends.indexOf(name.toUpperCase());
+  let checkFriend = friends.includes(name.toUpperCase());
+
+  if (checkFriend == true) {
+    console.log(`You remove ${friends.splice(indexofFriend, 1)}`);
+  } else {
+    console.log("The name is not found in the friend list.");
   }
 }
